62b004a5c5db09035d017a5277800256776b1ca3
Xoul <devxoul@gmail.com>
2012-01-18 00:44:38 +0900

    Initial Commit

---
+++ I'm Traveling/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  I'm Traveling
+//
+//  Created by 전 수열 on 12. 1. 18..
+//  Copyright (c) 2012년 Joyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ I'm Traveling/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  I'm Traveling
+//
+//  Created by 전 수열 on 12. 1. 18..
+//  Copyright (c) 2012년 Joyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
